40cd227145f76a675cd5ca21352577fe83ebbb40
claudiaaziz <claudiahannahh@gmail.com>
2023-11-08 11:46:11 -0500

    Add User Table And Model

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+  has_secure_password
+  before_validation :ensure_session_token
+
+  validates :username, 
+    uniqueness: true, 
+    length: { in: 3..30 }, 
+    format: { without: URI::MailTo::EMAIL_REGEXP, message:  "Username can't be an email" }
+  validates :email, 
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
+
+  def self.find_by_credentials(credential, password)
+    field = URI::MailTo::EMAIL_REGEXP.match(credential) ? :email : :username
+    user = User.find_by(field => credential)
+    user&.authenticate(password)
+  end
+
+  def reset_session_token!
+    self.update!(session_token: generate_unique_session_token)
+    self.session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+    loop do
+      token = SecureRandom.base64
+      return token unless User.exists?(session_token: token)
+    end
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+end
