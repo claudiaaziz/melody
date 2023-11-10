ff1f5446b8510f582b9ccd27b36a6a52214321ff
claudiaaziz <claudiahannahh@gmail.com>
2023-11-10 14:33:20 -0500

    Add css to nav, LandingPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import Navigation from "./components/Navigation";
+import LandingPage from "./components/LandingPage";
 
 const App = () => {
   return (
     <>
-      <Navigation />
       <Switch>
+        <Route path="/" exact>
+          <Navigation />
+          <LandingPage />
+        </Route>
         <Route path="/login">
           <LoginFormPage />
         </Route>
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
 
 const Navigation = () => {
   const currentUser = useSelector((state) => state.session.user);
@@ -19,15 +20,24 @@ const Navigation = () => {
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
-        {sessionLinks}
-      </li>
-    </ul>
+    <div className="NavBar">
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to="/signup" className="signup">
+            Sign Up
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className="login">
+            Log In
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
-}
+};
 
 export default Navigation;
