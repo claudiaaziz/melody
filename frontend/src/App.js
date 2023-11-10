a84adf464886a9885df705d2858504f7213025f7
claudiaaziz <claudiahannahh@gmail.com>
2023-11-10 18:16:03 -0500

    Fix Nav css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignupFormPage";
+import LoginFormPage from "./components/LandingPage/LoginFormPage";
+import SignupFormPage from "./components/LandingPage/SignupFormPage";
 import Navigation from "./components/Navigation";
 import LandingPage from "./components/LandingPage";
 
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -7,35 +7,29 @@ import "./Navigation.css";
 const Navigation = () => {
   const currentUser = useSelector((state) => state.session.user);
 
-  let sessionLinks;
-  if (currentUser) {
-    sessionLinks = <ProfileButton user={currentUser} />;
-  } else {
-    sessionLinks = (
-      <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </>
-    );
-  }
-
   return (
     <div className="NavBar">
-      <NavLink exact to="/">
-        Home
-      </NavLink>
-      <ul>
-        <li>
-          <NavLink to="/signup" className="signup">
-            Sign Up
+      {currentUser ? (
+        <ProfileButton user={currentUser} />
+      ) : (
+        <>
+          <NavLink exact to="/">
+            Home
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" className="login">
-            Log In
-          </NavLink>
-        </li>
-      </ul>
+          <ul>
+            <li>
+              <NavLink to="/signup" className="signup">
+                Sign Up
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/login" className="login">
+                Log In
+              </NavLink>
+            </li>
+          </ul>
+        </>
+      )}
     </div>
   );
 };
