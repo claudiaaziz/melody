eaf999ef13de3622724861655e76923aa09707b8
claudiaaziz <claudiahannahh@gmail.com>
2023-11-11 21:56:19 -0500

    Clean up code base

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -9,27 +9,27 @@ const Navigation = () => {
   const currentUser = useSelector((state) => state.session.user);
 
   return (
-    <div className="NavBar">
-      {currentUser ? (
-        <ProfileMenu />
-      ) : (
-        <>
-          <MelodyLogo />
-          <ul>
-            <li>
-              <NavLink to="/signup" className="signup">
-                Sign Up
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/login" className="login">
-                Log In
-              </NavLink>
-            </li>
-          </ul>
-        </>
-      )}
-    </div>
+    <div className="navBar">
+        {currentUser ? (
+          <ProfileMenu />
+        ) : (
+          <>
+            <MelodyLogo />
+            <ul>
+              <li>
+                <NavLink to="/signup" className="signup">
+                  Sign Up
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/login" className="login">
+                  Log In
+                </NavLink>
+              </li>
+            </ul>
+          </>
+        )}
+      </div>
   );
 };
 
