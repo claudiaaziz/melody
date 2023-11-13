a64397d918f5625be6abeb68b86d66b0bf34c695
claudiaaziz <claudiahannahh@gmail.com>
2023-11-13 13:24:11 -0500

    Fix error with last push

--- frontend/src/components/AlbumIndex/index.js
+++ frontend/src/components/AlbumIndex/index.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./AlbumIndex.css";
+import { fetchAlbums, getAlbums } from "../../store/albums";
 
 const AlbumIndex = () => {
+  const dispatch = useDispatch();
+  const albums = useSelector(getAlbums);
+
+  useEffect(() => {
+    dispatch(fetchAlbums());
+  }, []);
+
   return (
     <div className="albumIndex">
-      <h1>Album Index</h1>
+      {albums.map((album) => (
+        <div key={album.id} className="album">
+          <img src={album.albumCoverUrl} alt="" />
+          <h2>{album.title}</h2>
+          <p className="artistName">{album.artistName}</p>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ frontend/src/store/albums.js
@@ -0,0 +1,28 @@
+export const RECEIVE_ALBUMS = `posts/RECEIVE_ALBUMS`;
+
+const receiveAlbums = (albums) => ({
+  type: RECEIVE_ALBUMS,
+  albums,
+});
+
+export const getAlbums = (state) => state.albums ? Object.values(state.albums) : []
+
+export const fetchAlbums = () => async (dispatch) => {
+  const res = await fetch("/api/albums");
+
+  if (res.ok) {
+    const albums = await res.json();
+    dispatch(receiveAlbums(albums));
+  }
+};
+
+const albumsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_ALBUMS:
+      return { ...action.albums };
+    default:
+      return state;
+  }
+};
+
+export default albumsReducer;
