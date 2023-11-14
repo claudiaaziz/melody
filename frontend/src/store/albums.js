3dd852891c7aa7987b47193298e8267824b65061
claudiaaziz <claudiahannahh@gmail.com>
2023-11-14 16:33:01 -0500

    Add action and thunk for album show

--- frontend/src/store/albums.js
+++ frontend/src/store/albums.js
@@ -1,14 +1,27 @@
 export const RECEIVE_ALBUMS = `posts/RECEIVE_ALBUMS`;
+export const RECEIVE_ALBUM = `posts/RECEIVE_ALBUM`;
 
 const receiveAlbums = (albums) => ({
   type: RECEIVE_ALBUMS,
   albums,
 });
+const receiveAlbum = (album) => ({
+  type: RECEIVE_ALBUM,
+  album,
+});
 
 export const getAlbums = (state) => (state.albums ? state.albums : []);
+export const getAlbum = (albumId) => state => (state.albums ? state.albums[albumId] : null);
 
 export const fetchAlbums = () => async (dispatch) => {
   const res = await fetch("/api/albums");
+
+  const albums = await res.json();
+  dispatch(receiveAlbums(albums));
+};
+
+export const fetchAlbum = () => async (dispatch) => {
+  const res = await fetch(`/api/albums/${}`);
 
   const albums = await res.json();
   dispatch(receiveAlbums(albums));
