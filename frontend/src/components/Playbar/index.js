95b0383bccc62cb7853a0104c389bcbcd7888603
claudiaaziz <claudiahannahh@gmail.com>
2023-11-17 16:31:56 -0500

    Fix playbar positioning

--- frontend/src/components/Playbar/index.js
+++ frontend/src/components/Playbar/index.js
@@ -2,15 +2,31 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Actions from "./Actions";
 import AudioPlayer from "../Playbar/AudioPlayer";
-import VolumeControl from "../Playbar/VolumeControl"; 
+import VolumeControl from "../Playbar/VolumeControl";
 import "./Playbar.css";
 
 const Playbar = () => {
   const isPlaying = useSelector((state) => state.playbar.isPlaying);
   const currentSong = useSelector((state) => state.playbar.currentSong);
+  const albumCover = useSelector(
+    (state) => state.albums[currentSong?.id]?.albumCoverUrl
+  );
+  const artistName = useSelector(
+    (state) => state.albums[currentSong?.id]?.artistName
+  );
 
   return (
     <div className="playbar">
+      {currentSong ? (
+        <div className="currentSongContainer">
+          <img src={albumCover} alt="" className="albumCover" />
+          <div className="currentSongDetails">
+            <span className="currentSongTitle">{currentSong.title}</span>
+            <span className="currentSongArtistName">{artistName}</span>
+          </div>
+        </div>
+      ) : null}
+
       <Actions isPlaying={isPlaying} currentSong={currentSong} />
       <AudioPlayer />
       <VolumeControl />
