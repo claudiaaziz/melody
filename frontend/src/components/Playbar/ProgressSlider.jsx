7c2e75a514a19b7e7a759296b43189c068e4b616
claudiaaziz <claudiahannahh@gmail.com>
2023-11-18 10:52:20 -0500

    Implement UI that indicates the song length/ how much time has been played

--- frontend/src/components/Playbar/ProgressSlider.jsx
+++ frontend/src/components/Playbar/ProgressSlider.jsx
@@ -5,29 +5,37 @@ import { updateProgress } from "../../store/playbar";
 const ProgressSlider = ({ audioRef }) => {
   const dispatch = useDispatch();
   const progress = useSelector((state) => state.playbar.progress || 0);
+  const duration = audioRef?.current ? audioRef.current.duration || 0 : 0;
 
-  // handle changes in the progress slider
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    return `${String(minutes)}:${String(seconds).padStart(2, "0")}`;
+  };
+
   const handleProgressChange = useCallback((e) => {
     const newProgress = parseFloat(e.target.value);
     dispatch(updateProgress(newProgress));
 
-    // if audioRef exists and has a current property
-    if (audioRef && audioRef.current) {
-      // update the currentTime property with new progress
-      audioRef.current.currentTime = newProgress; 
+    if (audioRef?.current) {
+      audioRef.current.currentTime = newProgress;
     }
-  }, [audioRef, dispatch]); 
+  }, [audioRef, dispatch]);
 
   return (
-    <input
-      type="range"
-      min="0"
-      max={audioRef?.current ? audioRef.current.duration || 0 : 0}
-      step="0.01"
-      value={progress}
-      onChange={handleProgressChange}
-      className="progressSlider"
-    />
+    <div className="progressSliderContainer">
+      <span className="progressTime">{formatTime(progress)}</span>
+      <input
+        type="range"
+        min="0"
+        max={duration}
+        step="0.01"
+        value={progress}
+        onChange={handleProgressChange}
+        className="progressSlider"
+      />
+      <span className="progressTime">{formatTime(duration)}</span>
+    </div>
   );
 };
 
