aa8195cc90426863a8c3fb489b69caa85b59d7ff
claudiaaziz <claudiahannahh@gmail.com>
2023-11-17 20:57:12 -0500

    Focus on playbar functionality

--- frontend/src/components/Playbar/index.js
+++ frontend/src/components/Playbar/index.js
@@ -4,13 +4,9 @@ import Actions from "./Actions";
 import AudioPlayer from "../Playbar/AudioPlayer";
 import VolumeControl from "../Playbar/VolumeControl";
 import "./Playbar.css";
-import MelodyLogo from "../LogoAndSVGS/melodyLogo";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-import { getAlbum } from "../../store/albums";
 
 const Playbar = () => {
   const isPlaying = useSelector((state) => state.playbar.isPlaying);
-  const { albumId } = useParams();
   let currentSong = useSelector((state) => state.playbar.currentSong);
 
   return (
