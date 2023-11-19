7a7d4ff82ee5e83d05910604b479d02283cf2250
claudiaaziz <claudiahannahh@gmail.com>
2023-11-19 12:45:50 -0500

    Finish CSS for signupmodal

--- frontend/src/components/SignupAndLogin/Modal/index.js
+++ frontend/src/components/SignupAndLogin/Modal/index.js
@@ -1,22 +1,29 @@
 import React from "react";
 import "./SignupModal.css";
+import {getAlbum} from "../../../store/albums"
+import {useSelector} from "react-redux"
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const SignUpModal = ({ onClose }) => {
+  const {albumId} = useParams()
+  const album = useSelector(getAlbum(albumId))
+
   return (
     <div className="modalOverlay" onClick={onClose}>
       <div className="modal">
-        <div className="modalHeader">
+        <img className="signUpModalImg" src={album.albumCoverUrl} alt="" />
+        <div className="modalContent">
           <h3>Start listening with a free Spotify account</h3>
+          <button type="submit" className="signupModalBtn">
+            <Link to="/signup" className="signupModalSignupLink">
+              Sign up free
+            </Link>
+          </button>
+          <p className="signupModalLoginLink">
+            Already have an account? <Link to="/login">Log in</Link>
+          </p>
         </div>
-        <button type="submit" className="signupModalBtn">
-          <Link to="/signup" className="signupModalSignupLink">
-            Sign up free
-          </Link>
-        </button>
-        <p className="signupModalLoginLink">
-          Already have an account? <Link to="/login">Log in</Link>
-        </p>
       </div>
       <button onClick={onClose} className="signupModalCloseBtn">
         Close
