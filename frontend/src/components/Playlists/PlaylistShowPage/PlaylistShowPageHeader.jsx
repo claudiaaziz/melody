c4aa2a00293d6aff5e45db6950cb9251f27756bd
claudiaaziz <claudiahannahh@gmail.com>
2023-11-24 13:54:15 -0500

    Implement dropdown/ css for edit playlist details

--- frontend/src/components/Playlists/PlaylistShowPage/PlaylistShowPageHeader.jsx
+++ frontend/src/components/Playlists/PlaylistShowPage/PlaylistShowPageHeader.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import playlistCover from "../../../static/images/playlistCover.png";
 import { ReactComponent as DotsIcon } from "../../../static/LogoAndSVGS/dots.svg";
+import { ReactComponent as EditPlaylistIcon } from "../../../static/LogoAndSVGS/playlists/editPlaylist.svg";
 
 const PlaylistShowPageHeader = ({ playlist, currentUser }) => {
+  const [isEditPlaylistDropdownOpen, setIsEditPlaylistDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsEditPlaylistDropdownOpen(!isEditPlaylistDropdownOpen);
+  };
+
   return (
     <>
-    <div className="playlistShowHeader">
-      <img src={playlistCover} alt="" className="playlistShowHeaderImg" />
-      <div className="playlistDetails">
-        <p>Playlist</p>
-        <h2>{playlist.name}</h2>
-        <div>
-          <span>{currentUser.username}</span>
+      <div className="playlistShowHeader">
+        <img src={playlistCover} alt="" className="playlistShowHeaderImg" />
+        <div className="playlistDetails">
+          <p>Playlist</p>
+          <h2>{playlist.name}</h2>
+          <div>
+            <span>{currentUser.username}</span>
+          </div>
         </div>
       </div>
-    </div>
-      <DotsIcon className="dotsIcon"/>
+      <div className="dotsContainer">
+        <DotsIcon className="dotsIcon" onClick={toggleDropdown} />
+        {isEditPlaylistDropdownOpen && (
+          <div className="editPlaylistDropdown">
+            <EditPlaylistIcon/>
+            <p>Edit Details</p>
+          </div>
+        )}
+      </div>
     </>
   );
 };
 
 export default PlaylistShowPageHeader;
-
