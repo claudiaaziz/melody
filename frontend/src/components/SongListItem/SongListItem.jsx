d7d3ee818abdfe30bfe7cd2a8473bc9e7161a6d0
claudiaaziz <claudiahannahh@gmail.com>
2023-11-24 19:04:09 -0500

    Add Duration of songs for SongListItem

--- frontend/src/components/SongListItem/SongListItem.jsx
+++ frontend/src/components/SongListItem/SongListItem.jsx
@@ -1,7 +1,28 @@
-import React from "react";
-import "./SongListItem.css"
+import React, { useState, useEffect } from "react";
+import "./SongListItem.css";
 
 const SongListItem = ({ artistName, song, songNum, onClick }) => {
+
+  // get song duration from AWS
+  const [duration, setDuration] = useState(null);
+
+  const fetchSongDuration = async (url) => {
+    const audio = new Audio(url);
+    audio.addEventListener("loadedmetadata", () => {
+      setDuration(audio.duration);
+    });
+  };
+
+  useEffect(() => {
+    if (song.songUrl) fetchSongDuration(song.songUrl);
+  }, [song.songUrl]);
+
+  const formatDuration = (duration) => {
+    const minutes = Math.floor(duration / 60);
+    const seconds = Math.floor(duration % 60);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
   return (
     <ul className="songListItem" onClick={onClick}>
       <li>
@@ -10,6 +31,7 @@ const SongListItem = ({ artistName, song, songNum, onClick }) => {
           <div className="songTitle">{song.title}</div>
           <div className="artistName">{artistName}</div>
         </div>
+        <div className="songDuration">{formatDuration(duration)}</div>
       </li>
     </ul>
   );
