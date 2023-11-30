9ae268807e411f1104ab97371261e315c6f288db
claudiaaziz <claudiahannahh@gmail.com>
2023-11-29 20:00:35 -0500

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-require "open-uri"
+  require "open-uri"
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+  # This file should contain all the record creation needed to seed the database with its default values.
+  # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+  #
+  # Examples:
+  #
+  #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+  #   Character.create(name: "Luke", movie: movies.first)
 
-# ApplicationRecord.transaction do 
-puts "Destroying tables..."
+  # ApplicationRecord.transaction do 
+  puts "Destroying tables..."
   Playlist.destroy_all
   Song.destroy_all
   Album.destroy_all
@@ -20,83 +20,43 @@ puts "Destroying tables..."
   ApplicationRecord.connection.reset_pk_sequence!('users')
   ApplicationRecord.connection.reset_pk_sequence!('albums')
   ApplicationRecord.connection.reset_pk_sequence!('playlists')
+  ApplicationRecord.connection.reset_pk_sequence!('songs')
 
   puts "Creating guest user..."
-  User.create!(
+  guest = User.create!(
     username: 'guest',
     email: 'guest@guest.com',
     password: 'guestpassword'
   )
 
   puts "Creating artists..."
-  Artist.create!(
-    name: "Sami Yusuf"
-  )
-  Artist.create!(
-    name: "Joseph Attieh"
-  )
-  Artist.create!(
-    name: "Mesut Kurtis"
-  )
-  Artist.create!(
-    name: "Yara"
-  )
-  Artist.create!(
-    name: "Najwa Karam"
-  )
-  Artist.create!(
-    name: "Haife Wehbe"
-  )
+  artists = [
+    { name: "Sami Yusuf" },
+    { name: "Joseph Attieh" },
+    { name: "Mesut Kurtis" },
+    { name: "Yara" },
+    { name: "Najwa Karam" },
+    { name: "Haife Wehbe" }
+  ]
+
+  all_artists = []
+  artists.each { |artist| all_artists << Artist.create!(artist) }
 
   puts "Creating albums..."
-  Album.create!(
-    title: "Al-Mu'allim",
-    artist_id: 1,
-    release_year: 2005,
-    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Sami_Yusuf.png'
-  )
-  Album.create!(
-    title: "Hobb W Mkattar",
-    artist_id: 2,
-    release_year: 2015,
-    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Joseph_Attieh.png'
-  )
-  Album.create!(
-    title: "Tabassam",
-    artist_id: 3,
-    release_year: 2015,
-    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Mesut+Kurtis.png'
-  )
-  Album.create!(
-    title: "Ya Ayech Bi Oyouni",
-    artist_id: 4,
-    release_year: 2016,
-    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Yara.png'
-  )
-  Album.create!(
-    title: "هلليله مافي نوم",
-    artist_id: 5,
-    release_year: 2011,
-    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Najwa+Karam.png'
-  )
-  Album.create!(
-    title: "Sapiential",
-    artist_id: 1,
-    release_year: 2020,
-    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Sapiential.png'
-  )
-  Album.create!(
-    title: "Hawwa",
-    artist_id: 6,
-    release_year: 2018,
-    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Hawwa.png'
-  )
-  Album.create!(
-    title: "My Ummah",
-    artist_id: 1,
-    release_year: 2005,
-    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/My+Ummah.png'
-  )
+  albums= [
+    { title: "Al-Mu'allim", artist_id: artists[0].id, release_year: 2005, cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Sami_Yusuf.png' },
+    { title: "Hobb W Mkattar", artist_id: artists[1].id, release_year: 2015, cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Joseph_Attieh.png' },
+    { title: "Tabassam", artist_id: artists[2].id, release_year: 2015, cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Mesut+Kurtis.png' },
+    { title: "Ya Ayech Bi Oyouni", artist_id: artists[3].id, release_year: 2016, cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Yara.png' },
+    { title: "هلليله مافي نوم", artist_id: artists[4].id, release_year: 2011, cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Najwa+Karam.png' },
+    { title: "Sapiential", artist_id: artists[0].id, release_year: 2020, cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Sapiential.png' },
+    { title: "Hawwa", artist_id: artists[5].id, release_year: 2018, cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Hawwa.png' },
+    { title: "My Ummah", artist_id: artists[0].id, release_year: 2005, cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/My+Ummah.png' }
+  ]
+
+  all_albums = []
+  albums.each { |album| all_albums << Album.create!(album) }
+
 
   puts "Creating songs..."
   # Al-Mu'allim
@@ -107,37 +67,37 @@ puts "Destroying tables..."
   )
   Song.create!(
     title: "Who Is The Loved One?",
-    album_id: 1,
+    album_id: al_muallim.id,
     song_url: "song2"
   )
   Song.create!(
     title: "The Cave Of Hira",
-    album_id: 1,
+    album_id: al_muallim.id,
     song_url: "song3"
   )
   Song.create!(
     title: "Allahu",
-    album_id: 1,
+    album_id: al_muallim.id,
     song_url: "song4"
   )
   Song.create!(
     title: "The Creator",
-    album_id: 1,
+    album_id: al_muallim.id,
     song_url: "song5"
   )
   Song.create!(
     title: "Meditation",
-    album_id: 1,
+    album_id: al_muallim.id,
     song_url: "song6"
   )
   Song.create!(
     title: "Ya Mustafa",
-    album_id: 1,
+    album_id: al_muallim.id,
     song_url: "song7"
   )
   Song.create!(
     title: "Supplication",
-    album_id: 1,
+    album_id: al_muallim.id,
     song_url: "song8"
   )
   # Hobb W Mkattar
@@ -242,8 +202,6 @@ puts "Destroying tables..."
     album_id: 3,
     song_url: "song11"
   )
-  puts "Done!"
-# end
 
 # test Sapiential
   Song.create!(
@@ -291,3 +249,5 @@ puts "Destroying tables..."
   album_id: 6,
   song_url: "song9"
   )
+
+  puts "Done!"
