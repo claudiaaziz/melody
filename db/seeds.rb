6242d1cddf33ba92b1617e5560ebd20959f41555
claudiaaziz <claudiahannahh@gmail.com>
2023-11-29 20:23:02 -0500

    Fix song seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,109 +1,97 @@
-require "open-uri"
+  require "open-uri"
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+  # This file should contain all the record creation needed to seed the database with its default values.
+  # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+  #
+  # Examples:
+  #
+  #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+  #   Character.create(name: "Luke", movie: movies.first)
 
-# ApplicationRecord.transaction do 
-puts "Destroying tables..."
-Playlist.destroy_all
-Song.destroy_all
-Album.destroy_all
-Artist.destroy_all
-User.destroy_all
+  # ApplicationRecord.transaction do 
+  puts "Destroying tables..."
+  Playlist.destroy_all
+  Song.destroy_all
+  Album.destroy_all
+  Artist.destroy_all
+  User.destroy_all
 
-ApplicationRecord.connection.reset_pk_sequence!('artists')
-ApplicationRecord.connection.reset_pk_sequence!('users')
-ApplicationRecord.connection.reset_pk_sequence!('albums')
-ApplicationRecord.connection.reset_pk_sequence!('playlists')
-ApplicationRecord.connection.reset_pk_sequence!('songs')
+  ApplicationRecord.connection.reset_pk_sequence!('artists')
+  ApplicationRecord.connection.reset_pk_sequence!('users')
+  ApplicationRecord.connection.reset_pk_sequence!('albums')
+  ApplicationRecord.connection.reset_pk_sequence!('playlists')
+  ApplicationRecord.connection.reset_pk_sequence!('songs')
 
-puts "Creating guest user..."
-guest = User.create!(
-  username: 'guest',
-  email: 'guest@guest.com',
-  password: 'guestpassword'
-)
+  puts "Creating guest user..."
+  guest = User.create!(
+    username: 'guest',
+    email: 'guest@guest.com',
+    password: 'guestpassword'
+  )
 
-puts "Creating artists..."
-sami_yusuf = Artist.create!(name: "Sami Yusuf")
-joseph_attieh = Artist.create!(name: "Joseph Attieh")
-mesut_kurtis = Artist.create!(name: "Mesut Kurtis")
-yara = Artist.create!(name: "Yara")
-najwa_karam = Artist.create!(name: "Najwa Karam")
-haife_wehbe = Artist.create!(name: "Haife Wehbe")
+  puts "Creating artists..."
+  sami_yusuf = Artist.create!(name: "Sami Yusuf")
+  joseph_attieh = Artist.create!(name: "Joseph Attieh")
+  mesut_kurtis = Artist.create!(name: "Mesut Kurtis")
+  yara = Artist.create!(name: "Yara")
+  najwa_karam = Artist.create!(name: "Najwa Karam")
+  haife_wehbe = Artist.create!(name: "Haife Wehbe")
 
-puts "Creating albums..."
-al_muallim = Album.create!(
-  title: "Al-Mu'allim",
-  artist_id: sami_yusuf.id,
-  release_year: 2005,
-  album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Sami_Yusuf.png'
-)
-
-hobb_w_mkattar = Album.create!(
-  title: "Hobb W Mkattar",
-  artist_id: joseph_attieh.id,
-  release_year: 2015,
-  album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Joseph_Attieh.png'
-)
-
-tabassam = Album.create!(
-  title: "Tabassam",
-  artist_id: mesut_kurtis.id,
-  release_year: 2015,
-  album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Mesut+Kurtis.png'
-)
-
-ya_ayech_bi_oyouni = Album.create!(
-  title: "Ya Ayech Bi Oyouni",
-  artist_id: yara.id,
-  release_year: 2016,
-  album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Yara.png'
-)
-
-hillelha_mafi_nawm = Album.create!(
-  title: "هلليله مافي نوم",
-  artist_id: najwa_karam.id,
-  release_year: 2011,
-  album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Najwa+Karam.png'
-)
-
-sapiential = Album.create!(
-  title: "Sapiential",
-  artist_id: sami_yusuf.id,
-  release_year: 2020,
-  album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Sapiential.png'
-)
-
-hawwa = Album.create!(
-  title: "Hawwa",
-  artist_id: haife_wehbe.id,
-  release_year: 2018,
-  album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Hawwa.png'
-)
-
-my_ummah = Album.create!(
-  title: "My Ummah",
-  artist_id: sami_yusuf.id,
-  release_year: 2005,
-  album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/My+Ummah.png'
-)
-
-
-
-
-
+  puts "Creating albums..."
+  al_muallim = Album.create!(
+    title: "Al-Mu'allim",
+    artist_id: sami_yusuf.id,
+    release_year: 2005,
+    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Sami_Yusuf.png'
+  )
+  hobb_w_mkattar = Album.create!(
+    title: "Hobb W Mkattar",
+    artist_id: joseph_attieh.id,
+    release_year: 2015,
+    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Joseph_Attieh.png'
+  )
+  tabassam = Album.create!(
+    title: "Tabassam",
+    artist_id: mesut_kurtis.id,
+    release_year: 2015,
+    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Mesut+Kurtis.png'
+  )
+  ya_ayech_bi_oyouni = Album.create!(
+    title: "Ya Ayech Bi Oyouni",
+    artist_id: yara.id,
+    release_year: 2016,
+    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Yara.png'
+  )
+  hillelha_mafi_nawm = Album.create!(
+    title: "هلليله مافي نوم",
+    artist_id: najwa_karam.id,
+    release_year: 2011,
+    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Najwa+Karam.png'
+  )
+  sapiential = Album.create!(
+    title: "Sapiential",
+    artist_id: sami_yusuf.id,
+    release_year: 2020,
+    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Sapiential.png'
+  )
+  hawwa = Album.create!(
+    title: "Hawwa",
+    artist_id: haife_wehbe.id,
+    release_year: 2018,
+    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/Hawwa.png'
+  )
+  my_ummah = Album.create!(
+    title: "My Ummah",
+    artist_id: sami_yusuf.id,
+    release_year: 2005,
+    album_cover_url: 'https://melody-seeds.s3.us-east-2.amazonaws.com/My+Ummah.png'
+  )
 
   puts "Creating songs..."
   # Al-Mu'allim
   Song.create!(
     title: "Al-Mu'allim",
-    album_id: 1,
+    album_id: al_muallim.id,
     song_url: "song1"
   )
   Song.create!(
@@ -144,151 +132,150 @@ my_ummah = Album.create!(
   # Hobb W Mkattar
   Song.create!(
     title: "Hob W Mkattar",
-    album_id: 2,
+    album_id: hobb_w_mkattar.id,
     song_url: "song1"
   )
   Song.create!(
     title: "Min Jdid",
-    album_id: 2,
+    album_id: hobb_w_mkattar.id,
     song_url: "song2"
   )
   Song.create!(
     title: "Eiwiha",
-    album_id: 2,
+    album_id: hobb_w_mkattar.id,
     song_url: "song3"
   )
   Song.create!(
     title: "La Tkhallini",
-    album_id: 2,
+    album_id: hobb_w_mkattar.id,
     song_url: "song4"
   )
   Song.create!(
     title: "Kel Ma El Masa Bi Tol",
-    album_id: 2,
+    album_id: hobb_w_mkattar.id,
     song_url: "song5"
   )
   Song.create!(
     title: "Dam3ato",
-    album_id: 2,
+    album_id: hobb_w_mkattar.id,
     song_url: "song6"
   )
   Song.create!(
     title: "Kilme Elak",
-    album_id: 2,
+    album_id: hobb_w_mkattar.id,
     song_url: "song7"
   )
   Song.create!(
     title: "Ya Kezabi",
-    album_id: 2,
+    album_id: hobb_w_mkattar.id,
     song_url: "song8"
   )
   Song.create!(
     title: "Weilak",
-    album_id: 2,
+    album_id: hobb_w_mkattar.id,
     song_url: "song9"
   )
   # Tabassam
   Song.create!(
     title: "Rouhi Fidak",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song1"
   )
   Song.create!(
     title: "Tabassam",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song2"
   )
   Song.create!(
     title: "Alhamdu Lillah",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song3"
   )
   Song.create!(
     title: "Adnani",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song4"
   )
   Song.create!(
     title: "Ghar Hira",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song5"
   )
   Song.create!(
     title: "Eidun Saeed Feat. Maher Zain",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song6"
   )
   Song.create!(
     title: "Assalatu Wassalamu",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song7"
   )
   Song.create!(
     title: "Ya Man Bihali",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song8"
   )
   Song.create!(
     title: "Farha",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song9"
   )
   Song.create!(
     title: "Ataytu Bithanbi",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song10"
   )
   Song.create!(
     title: "Du'a",
-    album_id: 3,
+    album_id: tabassam.id,
     song_url: "song11"
   )
-
 # test Sapiential
   Song.create!(
     title: "One",
-    album_id: 6,
+    album_id: sapiential.id,
     song_url: "https://melody-seeds.s3.us-east-2.amazonaws.com/Elvana+Gjata+-+POW.mp3"
   )
   Song.create!(
     title: "The Centre",
-    album_id: 6,
+    album_id: sapiential.id,
     song_url: "https://melody-seeds.s3.us-east-2.amazonaws.com/Mesut+Kurtis+Ya-Man-Bihali.mp3"
   )
   Song.create!(
     title: "Cadence",
-    album_id: 6,
+    album_id: sapiential.id,
     song_url: "https://melody-seeds.s3.us-east-2.amazonaws.com/Yara+Ma-Baaref.mp3"
   )
   Song.create!(
-  title: "no song 1",
-  album_id: 6,
-  song_url: "song4"
+    title: "no song 1",
+    album_id: sapiential.id,
+    song_url: "song4"
   )
   Song.create!(
-  title: "no song 2",
-  album_id: 6,
-  song_url: "song5"
+    title: "no song 2",
+    album_id: sapiential.id,
+    song_url: "song5"
   )
   Song.create!(
-  title: "no song 3",
-  album_id: 6,
-  song_url: "song6"
+    title: "no song 3",
+    album_id: sapiential.id,
+    song_url: "song6"
   )
   Song.create!(
-  title: "no song 4",
-  album_id: 6,
-  song_url: "song7"
+    title: "no song 4",
+    album_id: sapiential.id,
+    song_url: "song7"
   )
   Song.create!(
-  title: "no song 5",
-  album_id: 6,
-  song_url: "song8"
+    title: "no song 5",
+    album_id: sapiential.id,
+    song_url: "song8"
   )
   Song.create!(
-  title: "no song 6",
-  album_id: 6,
-  song_url: "song9"
+    title: "no song 6",
+    album_id: sapiential.id,
+    song_url: "song9"
   )
 
   puts "Done!"
