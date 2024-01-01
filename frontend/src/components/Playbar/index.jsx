3ee86046cca105259cef083b54fe28723fda46e3
claudiaaziz <claudiahannahh@gmail.com>
2023-12-31 23:54:32 -0500

    Add signed out banner functionality & CSS

--- frontend/src/components/Playbar/index.jsx
+++ frontend/src/components/Playbar/index.jsx
@@ -5,6 +5,7 @@ import VolumeControl from "./VolumeControl";
 import "./Playbar.css";
 import { getAlbum } from "../../store/albums";
 import { useSelector } from "react-redux";
+import "./SignedOutBanner.css"
 
 const Playbar = () => {
   const currentQueueIdx = useSelector((state) => state.playbar.currentQueueIdx);
@@ -17,34 +18,47 @@ const Playbar = () => {
   );
   const currentAlbumId = currentSong?.albumId
   const album = useSelector(getAlbum(currentAlbumId));
-
+  const currentUser = useSelector((state) => state.session.user)
+  
   return (
-    <div className="playbar">
-      {currentSongId ? (
-        <div className="currentSongContainer">
-          <img
-            src={album?.albumCoverUrl}
-            alt=""
-            className="currentSongAlbumCover"
-          />
-          <div className="currentSongDetails">
-            <span className="currentSongTitle">{currentSong?.title}</span>
-            <span className="currentSongArtistName">{album?.artistName}</span>
+    <>
+      <div className="playbar">
+        {currentSongId ? (
+          <div className="currentSongContainer">
+            <img
+              src={album?.albumCoverUrl}
+              alt=""
+              className="currentSongAlbumCover"
+            />
+            <div className="currentSongDetails">
+              <span className="currentSongTitle">{currentSong?.title}</span>
+              <span className="currentSongArtistName">{album?.artistName}</span>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="currentSongContainer">
-        </div>
-      )}
+        ) : (
+          <div className="currentSongContainer">
+          </div>
+        )}
 
-      <div className="actionsAndProgressSliderContainer">
-        <Actions />
-        <AudioPlayer />
+        <div className="actionsAndProgressSliderContainer">
+          <Actions />
+          <AudioPlayer />
+        </div>
+        <div className="volumeControlContainer">
+          <VolumeControl />
+        </div>
       </div>
-      <div className="volumeControlContainer">
-        <VolumeControl />
-      </div>
-    </div>
+      {!currentUser &&
+      <div className="signed-out-banner">
+        <div className="signed-out-banner-text-div">
+          <span>Preview of Spotify</span>
+          <h2>Sign up to get unlimited songs and podcasts with occasional ads. No credit card needed.</h2>
+        </div>
+        <div className="signed-out-banner-btn-div">
+        <button>Sign up free</button>
+        </div>
+      </div>}
+    </>
   );
 };
 
