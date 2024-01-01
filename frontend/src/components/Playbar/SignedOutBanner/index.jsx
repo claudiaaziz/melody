9504f4f5b8d69ed497f26c8ec2ef7d0ea213eaa8
claudiaaziz <claudiahannahh@gmail.com>
2024-01-01 16:36:31 -0500

    Update signoutbanner text

--- frontend/src/components/Playbar/SignedOutBanner/index.jsx
+++ frontend/src/components/Playbar/SignedOutBanner/index.jsx
@@ -7,7 +7,7 @@ const SignedOutBanner = () => {
   return (
     <div className="signed-out-banner" onClick={() => history.push("/signup")}>
       <div className="signed-out-banner-text-div">
-        <span>Preview of Spotify</span>
+        <span>Preview of Melody</span>
         <h2>Sign up to get unlimited songs and podcasts with occasional ads. No credit card needed.</h2>
       </div>
       <div className="signed-out-banner-btn-div">
