72aaf479b227f2e63230142d1b6a5a65615dfb7c
claudiaaziz <claudiahannahh@gmail.com>
2024-02-11 13:47:10 -0500

    Refactor AlbumShow

--- frontend/src/components/Albums/AlbumShow/AlbumSongListItem/AlbumSongListItem.jsx
+++ frontend/src/components/Albums/AlbumShow/AlbumSongListItem/AlbumSongListItem.jsx
@@ -1,33 +1,53 @@
 import { useState } from "react";
 import "./AlbumSongListItem.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchSongDuration } from "../../../../utils/fetchSongDuration";
 import { formatDuration } from "../../../../utils/formatDuration";
+import { playQueue } from "../../../../store/playbar"
+import SignUpModal from "../../../SignupAndLogin/SignUpModal";
 
-const AlbumSongListItem = ({ artistName, song, songNum, handleAlbumSongClick }) => {
+const AlbumSongListItem = ({ album, song, songNum }) => {
+  const dispatch = useDispatch();
   const currentQueueIdx = useSelector((state) => state.playbar.currentQueueIdx);
   const currentSongId = useSelector(
     (state) => state.playbar.queue[currentQueueIdx] === song.id
   );
+    
+    const [duration, setDuration] = useState(null);
+    song.songUrl && fetchSongDuration(song.songUrl, setDuration); 
+    
+  const currentUser = useSelector((state) => state.session.user);
+  const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false);
 
-  const [duration, setDuration] = useState(null);
-  song.songUrl && fetchSongDuration(song.songUrl, setDuration); 
+  const handleAlbumSongClick = (songId) => {
+    if (currentUser) {
+      const currentQueueIdx = album.albumSongs.indexOf(songId)
+      dispatch(playQueue(album.albumSongs, currentQueueIdx));
+    } else {
+      setIsSignUpModalOpen(true);
+    }
+  };
 
   return (
-    <ul className="songListItem" onClick={handleAlbumSongClick}>
-      <li>
-        <div className={`songNum ${currentSongId && "currentSongId"}`}>
-          {songNum}
-        </div>
-        <div className="songContent">
-          <div className={`songTitle ${currentSongId && "currentSongId"}`}>
-            {song.title}
+    <>
+      <ul className="songListItem" onClick={() => handleAlbumSongClick(song.id)}>
+        <li>
+          <div className={`songNum ${currentSongId && "currentSongId"}`}>
+            {songNum}
           </div>
-          <div className="artistName">{artistName}</div>
-        </div>
-        <div className="songDuration">{formatDuration(duration)}</div>
-      </li>
-    </ul>
+          <div className="songContent">
+            <div className={`songTitle ${currentSongId && "currentSongId"}`}>
+              {song.title}
+            </div>
+            <div className="artistName">{album.artistName}</div>
+          </div>
+          <div className="songDuration">{formatDuration(duration)}</div>
+        </li>
+      </ul>
+      {isSignUpModalOpen && (
+        <SignUpModal closeSignUpModal={() => setIsSignUpModalOpen(false)} />
+      )}
+    </>
   );
 };
 
--- frontend/src/components/Albums/AlbumShow/index.jsx
+++ frontend/src/components/Albums/AlbumShow/index.jsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import AlbumSongListItem from "./AlbumSongListItem/AlbumSongListItem.jsx";
 import AlbumShowHeader from "./AlbumShowHeader.jsx";
-import SignUpModal from "../../SignupAndLogin/SignUpModal/index.jsx";
 import { fetchAlbum, getAlbum } from "../../../store/albums.js";
 import { getSongs } from "../../../store/songs.js";
-import { playQueue } from "../../../store/playbar.js";
 import "./AlbumShow.css";
 
 const AlbumShow = () => {
@@ -17,30 +15,15 @@ const AlbumShow = () => {
     dispatch(fetchAlbum(albumId));
   }, [dispatch, albumId]);
   
-  const currentUser = useSelector((state) => state.session.user);
   const album = useSelector(getAlbum(albumId));
   const songs = useSelector(getSongs);
   
   const albumSongs = album?.albumSongs?.map((songId) => songs[songId]);
 
-  const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false);
-
-  // play album songs 
-  const handleAlbumSongClick = (songId) => {
-    if (currentUser) {
-      const currentQueueIdx = album.albumSongs.indexOf(songId)
-      dispatch(playQueue(album.albumSongs, currentQueueIdx));
-    } else {
-      setIsSignUpModalOpen(true);
-    }
-  };
-
   return (
     <div className="albumShowPage">
       {album && <AlbumShowHeader album={album} />}
-
       <hr />
-
         {album &&
           albumSongs
             .filter((song) => song) // filter out undefined songs 
@@ -49,13 +32,9 @@ const AlbumShow = () => {
                 key={song.id}
                 song={song}
                 songNum={idx + 1}
-                artistName={album.artistName}
-                handleAlbumSongClick={() => handleAlbumSongClick(song.id)}
+                album={album}
               />
             ))}
-        {isSignUpModalOpen && (
-          <SignUpModal closeSignUpModal={() => setIsSignUpModalOpen(false)} />
-        )}
       </div>
   );
 };
