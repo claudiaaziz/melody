8e8f66862a97c3d58805bda6228611e06617e2e2
claudiaaziz <claudiahannahh@gmail.com>
2024-08-06 22:28:10 -0400

    Cache setup just need to test more

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,6 +1,32 @@
 class Api::SongsController < ApplicationController 
-  def index
-    @songs = Song.all
+#   def index
+#     @songs = Song.all
+#   end
+
+# def index
+#     @songs = Rails.cache.fetch('all_songs') do
+#       Song.all.to_a
+#     end
+#     render json: @songs
+#   end
+
+#   def index
+#     @songs = Rails.cache.fetch('all_songs') do
+#       Rails.logger.info "Fetching songs from database"
+#       Song.all.to_a
+#     end
+#     Rails.logger.info "Fetching songs from cache" if Rails.cache.exist?('all_songs')
+#   end
+
+def index
+    if Rails.cache.exist?('all_songs')
+      Rails.logger.info "Fetching songs from cache"
+      @songs = Rails.cache.read('all_songs')
+    else
+      Rails.logger.info "Fetching songs from database"
+      @songs = Song.all.to_a
+      Rails.cache.write('all_songs', @songs)
+    end
   end
 
   def show
@@ -9,7 +35,7 @@ class Api::SongsController < ApplicationController
 
   private
 
-  def song_params
-    params.require(:album).permit(:title, :album_id, :song_url)
-  end
+#   def song_params
+#     params.require(:album).permit(:title, :album_id, :song_url)
+#   end
 end
